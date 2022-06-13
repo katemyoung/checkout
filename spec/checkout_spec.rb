a5b699da1168c44229af6f8ce6788bf690ba96f3
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-06-13 17:55:09 +0200

    Create Checkout class which accepts one argument on initialization

---
+++ spec/checkout_spec.rb
@@ -0,0 +1,9 @@
+require "checkout"
+
+RSpec.describe Checkout do
+  describe "on initalization of a new checkout" do
+    it "is expected to accpet promitional rules as an argument" do
+      expect(Checkout.itself).to respond_to(:new).with(1).argument
+    end 
+  end
+end
